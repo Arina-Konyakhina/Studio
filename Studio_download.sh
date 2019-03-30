6efd8b27c9640389685553e9efcd32ebae22c571
Arina-Konyakhina <43668643+Arina-Konyakhina@users.noreply.github.com>
2019-03-30 12:07:28 +0300

    Delete Studio_download.sh

--- Studio_download.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt update
-sudo apt install git
-git config —global user.name Arina-Konyakhina
-git config —global user.email arina1304@mail.ru
-git clone https://github.com/Arina-Konyakhina/Studio StudioClone
