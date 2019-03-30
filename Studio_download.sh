065cd8a3eaf2ba9ed31088eee7f4eb5adfa79a6b
Arina-Konyakhina <43668643+Arina-Konyakhina@users.noreply.github.com>
2019-03-30 12:07:54 +0300

    Add files via upload

---
+++ Studio_download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt update
+sudo apt install git
+git config —global user.name Arina-Konyakhina
+git config —global user.email arina1304@mail.ru
+git clone https://github.com/Arina-Konyakhina/Studio StudioClone
