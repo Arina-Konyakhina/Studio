7a0f12ae54cf9a0af654810a15e5dab176c8e4d7
Arina-Konyakhina <43668643+Arina-Konyakhina@users.noreply.github.com>
2019-03-30 12:51:11 +0300

    Add files via upload

---
+++ Git_remove.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+sudo apt-get remove git
+sudo rm -R StudioClone
---
+++ Studio_download.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt update
+sudo apt install git
+git config —global user.name Arina-Konyakhina
+git config —global user.email arina1304@mail.ru
+git clone https://github.com/Arina-Konyakhina/Studio StudioClone
+cd StudioClone
+chmod +x Install_Snowmix.sh
+chmod +x Git_remove.sh
+chmod +x MIDIInput.sh
+
+
