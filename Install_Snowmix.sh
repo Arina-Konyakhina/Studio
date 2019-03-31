355f1eda0d758a1effbe3a21b537e15be8a110c5
Arina-Konyakhina <43668643+Arina-Konyakhina@users.noreply.github.com>
2019-03-31 20:56:39 +0300

    Update Install_Snowmix.sh

--- Install_Snowmix.sh
+++ Install_Snowmix.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-sudo apt-get install ubuntu-desktop
-sudo reboot
-sudo apt-get install virtualbox-guest-dkms
-sudo reboot
+#sudo apt-get install virtualbox-guest-dkms
+#sudo reboot
 sudo apt-get install build-essential automake autoconf libtool g++ pkg-config         libsdl1.2-dev libpango1.0-dev libpng12-dev libosmesa6-dev freeglut3-dev
 sudo apt-get install tcl8.6 tk8.6 bwidget tcl8.6-dev
 mkdir tmp
