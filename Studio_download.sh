a132c58cab00f40812d2875d319aad48a843c14a
Arina-Konyakhina <43668643+Arina-Konyakhina@users.noreply.github.com>
2019-04-01 17:41:36 +0300

    Update Studio_download.sh

--- Studio_download.sh
+++ Studio_download.sh
@@ -19,7 +19,7 @@ sudo chmod +x in_audio
 sudo chmod +x out2
 sudo chmod +x rtsp2feed_alpha
 sudo chmod +x rtsp2feed_wo_alpha
-sudo chmod +x killsnowmix        !!!!!!!!!!!!!!!!!!!
+sudo chmod +x killsnowmix.sh
 
 #Download Snowmix and Gstreamer
 sudo cp Install_Snowmix.sh /
@@ -36,7 +36,7 @@ sudo cp in_audio /usr/local/bin/
 sudo cp out2 /usr/local/bin/
 sudo cp rtsp2feed_alpha /usr/local/bin/
 sudo cp rtsp2feed_wo_alpha /usr/local/bin/
-sudo cp killsnowmix /usr/local/bin/                !!!!!!!!!!!!!
+sudo cp killsnowmix.sh /usr/local/bin/
 sudo cp myscene.ini /usr/local/lib/Snowmix-0.5.1/ini
 sudo cp MIDIInput.sh /usr/local/bin/MIDIInput.sh
 
