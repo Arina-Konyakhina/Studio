ac5cad51c72294f1fa1971a403dd06d115a27767
Arina-Konyakhina <43668643+Arina-Konyakhina@users.noreply.github.com>
2019-04-03 20:14:15 +0300

    Update Studio_download.sh

--- Studio_download.sh
+++ Studio_download.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+
 mkdir Studio
 cd Studio
 sudo apt update
+
 #Git installing and clonning repo
 sudo apt install git
 git config —global user.name Arina-Konyakhina
 git config —global user.email arina1304@mail.ru
 git clone https://github.com/Arina-Konyakhina/Studio StudioClone
 cd StudioClone
+
 #Changing permissions
 sudo chmod +x Install_Snowmix.sh
 sudo chmod +x MIDIInput.sh
@@ -17,13 +20,16 @@ sudo chmod +x out2
 sudo chmod +x rtsp2feed_alpha
 sudo chmod +x rtsp2feed_wo_alpha
 sudo chmod +x killsnowmix.sh
+
 #Download Snowmix and Gstreamer
 sudo cp Install_Snowmix.sh /
 cd /
 ./Install_Snowmix.sh
+
 #Move to downloads directory
 cd "$(dirname "$(find / -name "StudioClone")")"
 cd StudioClone
+
 #Moving files to correct directories
 sudo cp myscene /usr/local/bin/
 sudo cp in_audio /usr/local/bin/
@@ -32,14 +38,17 @@ sudo cp rtsp2feed_alpha /usr/local/bin/
 sudo cp rtsp2feed_wo_alpha /usr/local/bin/
 sudo cp killsnowmix.sh /usr/local/bin/
 sudo cp myscene.ini /usr/local/lib/Snowmix-0.5.1/ini
+
 echo Studio is ready, use control unit to start studio
 sudo cp MIDIInput.sh /usr/local/bin/MIDIInput.sh
+
 # Compile unistalled element x264enc
 cd /tmp/gst-plugins-ugly-1.6.3
 sudo apt-get install libx264-dev
 sudo ./configure
 sudo make
 sudo make install
+
 # Moving midi script to autostart
 exit
 cd ~/.config
@@ -47,6 +56,7 @@ mkdir autostart
 cd "$(dirname "$(find / -name "StudioClone")")"
 cd StudioClone
 cp midi.desktop ~/.config/autostart/midi.desktop
+
 # Removing trash
 cd "$(dirname "$(find / -name "Studio")")"
 sudo rm -R Studio
